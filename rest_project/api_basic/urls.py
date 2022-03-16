f80eca8ef0a55748b1fb8cbed32d67a1dc7fcb3c
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-03-16 12:25:31 +0600

    GET, POST, PUT, DELETE API :grey_question:

--- rest_project/api_basic/urls.py
+++ rest_project/api_basic/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import article_list
+from .views import article_list, article_detail
 
 urlpatterns = [
     
-    path('article/', article_list),
-]
+    path('article/', article_list, name="article"),
+    path('detail/<int:pk>/', article_detail, name="detail"),
+] 
--- rest_project/rest_project/settings.py
+++ rest_project/rest_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'rest_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '623492',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
